86151e26c356d05d277b02bf70ca2eea7b08646e
Amar Baldewsing <088681@glr.nl>
2024-04-26 12:07:21 +0200

    producten

---
+++ src/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home - WheyTooFun</title>
+    <link rel="stylesheet" href="style/css/style.css">
+</head>
+<body>
+    <nav class="b-bottom BG-300">
+        <div class="wrapper con-nav flex-row-between">
+            <h1 class="logo">WheyTooFun</h1>
+            <ul class="links flex-row-between gap">
+                <li><a class="title" href="index.php">Home</a></li>
+                <li><a class="title" href="#">Over Ons</a></li>
+                <li><a class="title" href="pages/producten.php">Producten</a></li>
+                <li><a class="button" href="#">Log In</a></li>
+            </ul>
+        </div>
+    </nav>
+</body>
+</html>
---
+++ src/pages/producten.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Items - WheyTooFun</title>
+    <link rel="stylesheet" href="../style/css/style.css">
+</head>
+<body>
+    <nav class="b-bottom BG-300">
+        <div class="wrapper con-nav flex-row-between">
+            <h1 class="logo">WheyTooFun</h1>
+            <ul class="links flex-row-between gap">
+                <li><a class="title" href="../index.php">Home</a></li>
+                <li><a class="title" href="#">Over Ons</a></li>
+                <li><a class="title" href="producten.php">Producten</a></li>
+                <li><a class="button" href="#">Log In</a></li>
+            </ul>
+        </div>
+    </nav>
+    <main>
+        <div class="wrapper flex-row-between gap">
+            <div class="card">
+                <div style="background-image: url('https://cdn.shopify.com/s/files/1/0185/2846/9056/files/ImpactT-ShirtGSSandBrownA6A5Z-NBZS-0303.jpg?v=1711527969');" class="card-img">
+                    <span class="highlight">nieuw</span>
+                </div>
+                <div class="card-txt">
+                    <p class="card-title">Impact T-shirt</p>
+                    <p class="card-price">$30</p>
+                </div>
+            </div>
+            <div class="card">
+                <div style="background-image: url('https://cdn.shopify.com/s/files/1/0185/2846/9056/files/ImpactT-ShirtGSSandBrownA6A5Z-NBZS-0303.jpg?v=1711527969');" class="card-img">
+                    <span class="highlight">nieuw</span>
+                </div>
+                <div class="card-txt">
+                    <p class="card-title">Impact T-shirt</p>
+                    <p class="card-price">$30</p>
+                </div>
+            </div>
+            <div class="card">
+                <div style="background-image: url('https://cdn.shopify.com/s/files/1/0185/2846/9056/files/ImpactT-ShirtGSSandBrownA6A5Z-NBZS-0303.jpg?v=1711527969');" class="card-img">
+                    <span class="highlight">nieuw</span>
+                </div>
+                <div class="card-txt">
+                    <p class="card-title">Impact T-shirt</p>
+                    <p class="card-price">$30</p>
+                </div>
+            </div>
+            <div class="card">
+                <div style="background-image: url('https://cdn.shopify.com/s/files/1/0185/2846/9056/files/ImpactT-ShirtGSSandBrownA6A5Z-NBZS-0303.jpg?v=1711527969');" class="card-img">
+                    <span class="highlight">nieuw</span>
+                </div>
+                <div class="card-txt">
+                    <p class="card-title">Impact T-shirt</p>
+                    <p class="card-price">$30</p>
+                </div>
+            </div>
+            <div class="card">
+                <div style="background-image: url('https://cdn.shopify.com/s/files/1/0185/2846/9056/files/ImpactT-ShirtGSSandBrownA6A5Z-NBZS-0303.jpg?v=1711527969');" class="card-img">
+                    <span class="highlight">nieuw</span>
+                </div>
+                <div class="card-txt">
+                    <p class="card-title">Impact T-shirt</p>
+                    <p class="card-price">$30</p>
+                </div>
+            </div>
+            <div class="card">
+                <div style="background-image: url('https://cdn.shopify.com/s/files/1/0185/2846/9056/files/ImpactT-ShirtGSSandBrownA6A5Z-NBZS-0303.jpg?v=1711527969');" class="card-img">
+                    <span class="highlight">nieuw</span>
+                </div>
+                <div class="card-txt">
+                    <p class="card-title">Impact T-shirt</p>
+                    <p class="card-price">$30</p>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
